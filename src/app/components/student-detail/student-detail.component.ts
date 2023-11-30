62e7d8f2f026efbeefb565c77d00bc26e567d1cd
xalessandrox <formicale@hotmail.com>
2023-11-30 13:53:00 +0100

    component files created

---
+++ src/app/components/student-detail/student-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-student-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './student-detail.component.html',
+  styleUrl: './student-detail.component.scss'
+})
+export class StudentDetailComponent {
+
+}
